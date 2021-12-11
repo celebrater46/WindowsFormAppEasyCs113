55d2fe23556399b3ba9c33aab2a260be58fff53a
hideru <jota.16.stigma.world@gmail.com>
2021-12-11 18:03:06 +0900

    first commit

--- WindowsFormsAppEasyCs113/Form1.cs
+++ WindowsFormsAppEasyCs113/Form1.cs
@@ -3,7 +3,10 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
+using System.Net;
+using System.Net.Sockets;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -12,9 +15,29 @@ namespace WindowsFormsAppEasyCs113
 {
     public partial class Form1 : Form
     {
+        public static string HOST = "localhost";
+        public static int PORT = 10000;
+
         public Form1()
         {
             InitializeComponent();
+            IPHostEntry ih = Dns.GetHostEntry(HOST);
+
+            TcpListener tl = new TcpListener(ih.AddressList[0], PORT);
+            tl.Start();
+            
+            Console.WriteLine("Waiting...");
+            while (true)
+            {
+                TcpClient tc = tl.AcceptTcpClient();
+                StreamWriter sw = new StreamWriter(tc.GetStream());
+                sw.WriteLine("I am the server.");
+
+                sw.Flush();
+                sw.Close();
+                tc.Close();
+                break;
+            }
         }
     }
 }
